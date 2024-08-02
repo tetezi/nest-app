45aacac81b98f55582231f8911254536114f82b6
wwx <1913483156@qq.com>
2024-08-02 15:55:23 +0800

    1

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -17,31 +17,33 @@ import { ResolvePromisesInterceptor } from './interceptors/resolve-promises';
   providers: [
     {
       provide: APP_GUARD,
-      // 提供API密钥守卫，用于验证API请求
+      // 全局API密钥守卫，用于验证API请求
       useClass: ApiKeyGuard,
     },
 
     {
       provide: APP_INTERCEPTOR,
-      // 提供拦截器，用于解析Promise
+      // 全局拦截器，用于解析Promise
       useClass: ResolvePromisesInterceptor,
     },
 
     {
       provide: APP_INTERCEPTOR,
-      // 提供拦截器，用于包装响应
+      // 全局拦截器，用于包装响应
       useClass: WrapResponseInterceptor,
     },
-
-    {
-      provide: APP_INTERCEPTOR,
-      // 提供拦截器，用于处理请求超时
-      useClass: TimeoutInterceptor,
-    },
+    /**
+     * 暂时关闭超时拦截器，未确定覆盖超时方案
+     */
+    // {
+    //   provide: APP_INTERCEPTOR,
+    //   // 全局拦截器，用于处理请求超时
+    //   useClass: TimeoutInterceptor,
+    // },
 
     {
       provide: APP_PIPE,
-      // 提供管道，用于验证请求数据
+      // 全局管道，用于验证请求数据
       useFactory: () => {
         return new ValidationPipe(validationOptions);
       },
