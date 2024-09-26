0b2a6e87b1801de5cdf574f7efa6a05e8257e729
wwx <1913483156@qq.com>
2024-09-26 13:55:36 +0800

    <refactor>修改种子

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -66,7 +66,10 @@ async function initUser() {
 /**
  * 初始化菜单表
  */
-async function initMenu(extraWorkApplicationDynamicFormView) {
+async function initMenu({
+  extraWorkApplicationDynamicFormView,
+  textCollectionDynamicFormView,
+}) {
   /**
    * 创建管理员菜单
    */
@@ -100,6 +103,13 @@ async function initMenu(extraWorkApplicationDynamicFormView) {
               url: '/sys/admin/enum',
               isEnabled: true,
             },
+            {
+              name: '文本收录管理',
+              type: 'DynamicFormView',
+              dynamicFormViewId: textCollectionDynamicFormView.id,
+              routerPath: '/sys/admin/textCollection',
+              isEnabled: true,
+            },
           ],
         },
       },
@@ -128,8 +138,8 @@ async function initMenu(extraWorkApplicationDynamicFormView) {
             {
               name: '表单管理',
               type: 'View',
-              routerPath: '/sys/dynamic/form/v2',
-              url: '/sys/dynamic/form/v2',
+              routerPath: '/sys/dynamic/form',
+              url: '/sys/dynamic/form',
               isEnabled: true,
             },
             {
@@ -243,17 +253,17 @@ async function initDynamicForm() {
           colProps: {
             span: 6,
           },
-          component: 'ApiSelect',
+          component: 'EnumSelect',
           labelShow: {
             type: 'value',
             value: true,
           },
-          schemaKey: 'bce786b3-1517-40bf-abb6-8898224b86af',
+          schemaKey: 'df4139ef-7848-45bd-ad31-eb772c52c1df',
           labelWidth: {
             type: 'value',
           },
           componentProps:
-            "    return { \n        api: async (params) => {\n            const CategoryName = 'ExtraWorkApplicationType'\n            return ( await renderParams.apiMethods.getEnumCategory(CategoryName)).details\n        },\n        labelField: 'name',\n        valueField: 'value',\n    }",
+            "    return { \n        categoryName: 'ExtraWorkApplicationType',\n    }",
         },
         {
           name: '开始时间',
@@ -325,12 +335,111 @@ async function initDynamicForm() {
       ],
     },
   });
+  const textCollectionDynamicForm = await prisma.dynamicForm.create({
+    data: {
+      name: '文本收录',
+      labelPosition: 'top',
+      schemas: [
+        {
+          category: 'Input',
+          name: '角色',
+          component: 'Input',
+          schemaKey: '2df18c4a-3e3f-4833-af7d-d4e197dcd6e4',
+          ifShow: {
+            type: 'value',
+            value: true,
+          },
+          colProps: {
+            span: 6,
+          },
+          field: 'author',
+          label: '角色',
+          labelShow: {
+            type: 'value',
+            value: true,
+          },
+          labelWidth: {
+            type: 'value',
+          },
+        },
+        {
+          category: 'Input',
+          name: '出处',
+          component: 'Input',
+          schemaKey: '21191ab9-0dbb-43ed-bd5b-295b3663043f',
+          ifShow: {
+            type: 'value',
+            value: true,
+          },
+          colProps: {
+            span: 6,
+          },
+          field: 'source',
+          label: '出处',
+          labelShow: {
+            type: 'value',
+            value: true,
+          },
+          labelWidth: {
+            type: 'value',
+          },
+        },
+        {
+          category: 'Input',
+          name: '内容',
+          component: 'Input',
+          componentProps: "return {\n\ttype: 'textarea'\n}",
+          schemaKey: '7e13e24d-d6b7-451d-9fb3-cbee01dca549',
+          ifShow: {
+            type: 'value',
+            value: true,
+          },
+          colProps: {
+            span: 24,
+          },
+          field: 'text',
+          label: '内容',
+          labelShow: {
+            type: 'value',
+            value: true,
+          },
+          labelWidth: {
+            type: 'value',
+          },
+        },
+        {
+          category: 'Input',
+          name: '备注',
+          component: 'Input',
+          componentProps: "return {\n\ttype: 'textarea'\n}",
+          schemaKey: 'a8cd1807-60f0-4403-9492-64b2608acc28',
+          ifShow: {
+            type: 'value',
+            value: true,
+          },
+          colProps: {
+            span: 24,
+          },
+          field: 'description',
+          label: '备注',
+          labelShow: {
+            type: 'value',
+            value: true,
+          },
+          labelWidth: {
+            type: 'value',
+          },
+        },
+      ],
+    },
+  });
   return {
     extraWorkApplicationDynamicForm,
+    textCollectionDynamicForm,
   };
 }
 /**
- * 初始化轻代码表单表
+ * 初始化轻代码数据表
  */
 async function initDynamicTable(extraWorkApplicationTypeEnum) {
   const userDynamicTable = await prisma.dynamicTable.create({
@@ -448,11 +557,61 @@ async function initDynamicTable(extraWorkApplicationTypeEnum) {
       },
     },
   });
-  return { userDynamicTable, extraWorkApplicationDynamicTable };
+  const textCollectionDynamicTable = await prisma.dynamicTable.create({
+    data: {
+      name: '文本收录表',
+      tableName: 'TextCollection',
+      cols: {
+        create: [
+          {
+            name: 'author',
+            colType: 'String',
+            canQuery: true,
+            canWritable: true,
+          },
+          {
+            name: 'source',
+            colType: 'String',
+            canQuery: true,
+            canWritable: true,
+          },
+          {
+            name: 'text',
+            colType: 'String',
+            canQuery: true,
+            canWritable: true,
+          },
+          {
+            name: 'description',
+            colType: 'String',
+            canQuery: true,
+            canWritable: true,
+          },
+          {
+            name: 'createdAt',
+            colType: 'DateTime',
+            canQuery: true,
+            canWritable: false,
+          },
+          {
+            name: 'updatedAt',
+            colType: 'DateTime',
+            canQuery: true,
+            canWritable: false,
+          },
+        ],
+      },
+    },
+  });
+  return {
+    userDynamicTable,
+    extraWorkApplicationDynamicTable,
+    textCollectionDynamicTable,
+  };
 }
 async function initDynamicFormView(
-  extraWorkApplicationDynamicTable,
-  extraWorkApplicationDynamicForm,
+  { extraWorkApplicationDynamicTable, textCollectionDynamicTable },
+  { extraWorkApplicationDynamicForm, textCollectionDynamicForm },
 ) {
   const extraWorkApplicationDynamicFormView =
     await prisma.dynamicFormViewComp.create({
@@ -519,20 +678,166 @@ async function initDynamicFormView(
         ],
       },
     });
-  return { extraWorkApplicationDynamicFormView };
+  const textCollectionDynamicFormView = await prisma.dynamicFormViewComp.create(
+    {
+      data: {
+        name: '文本收录',
+        dataSourceType: 'DynamicTable',
+        dynamicTable: {
+          connect: { id: textCollectionDynamicTable.id },
+        },
+        formSourceType: 'DynamicForm',
+        dynamicForm: {
+          connect: { id: textCollectionDynamicForm.id },
+        },
+        tableColumns: [
+          {
+            prop: 'text',
+            label: '内容',
+            width: '',
+            transform: '',
+            showOverflowTooltip: false,
+          },
+          {
+            prop: 'author',
+            label: '角色',
+            width: '',
+            transform: '',
+            showOverflowTooltip: false,
+          },
+          {
+            prop: 'source',
+            label: '出处',
+            width: '',
+            transform: '',
+            showOverflowTooltip: false,
+          },
+          {
+            prop: 'description',
+            label: '备注',
+            width: '',
+            transform: '',
+            showOverflowTooltip: false,
+          },
+          {
+            prop: 'createdAt',
+            label: '创建时间',
+            width: '',
+            transform: '',
+            showOverflowTooltip: false,
+          },
+          {
+            prop: 'updatedAt',
+            label: '编辑时间',
+            width: '',
+            transform: '',
+            showOverflowTooltip: false,
+          },
+        ],
+      },
+    },
+  );
+  return { extraWorkApplicationDynamicFormView, textCollectionDynamicFormView };
+}
+async function initDynamicTableRecored() {
+  await prisma.textCollection.createMany({
+    data: [
+      {
+        source: '孤独摇滚',
+        text: '要是舍弃了个性，就跟死去没有两样',
+        author: '山田凉',
+      },
+      {
+        text: '世界上只有一种真正的英雄主义,那就是认清生活真相后依旧热爱生活',
+        author: '罗曼·罗兰',
+      },
+      {
+        text: '我认为教育小孩子”世界上没有黑暗“是一件非常糟糕的事情',
+        author: '虚渊玄',
+      },
+      {
+        source: '魔法少女小圆',
+        text: '太过善良有时候可能会换来更大的悲伤',
+        author: '巴麻美',
+      },
+      {
+        source: 'JOJO的奇妙冒险',
+        text: '人的成长就是战胜过去不成熟的自己',
+        author: '迪亚波罗',
+      },
+      {
+        source: '命运石之门',
+        text: '这一切都是命运石之门的选择',
+        author: '冈部伦太郎',
+      },
+      {
+        source: 'JOJO的奇妙冒险',
+        text: '两个囚徒从监狱的窗口里向外看，一个凝视着泥土，一个仰望着星空',
+      },
+      {
+        source: '进击的巨人',
+        text: '什么都无法舍弃的人，什么都改变不了',
+        author: '阿尔敏',
+      },
+      {
+        source: '弹丸论破',
+        text: '规矩，既是束缚，也是保护',
+      },
+      {
+        source: '弹丸论破',
+        text: '最终能幸存下来的人，既不是强者，也不是智者，而是能适应变化的人',
+      },
+      {
+        source: '魔法少女小圆',
+        text: '比希望更炙热，比绝望更深邃的，是爱啊',
+        author: '晓美焰',
+      },
+      {
+        source: '脑叶公司',
+        text: '即便这是施加于我的诅咒，我也会将它当成祝福一样热爱',
+      },
+      {
+        source: '脑叶公司',
+        text: '那么，正于此地，愿您找到想要的书',
+        author: '安吉拉',
+      },
+      {
+        source: '脑叶公司',
+        text: '拥抱过去，创造未来',
+      },
+      {
+        source: '阿里云',
+        text: '为了无法计算的价值',
+      },
+      {
+        source: '人狼村之谜',
+        text: '即便如此，也要追求狼的胜利',
+        author: '房石阳明',
+      },
+    ],
+  });
 }
 async function main() {
   const { testUser, adminUser } = await initUser();
   const { extraWorkApplicationTypeEnum } = await initEnum();
-  const { extraWorkApplicationDynamicForm } = await initDynamicForm();
-  const { userDynamicTable, extraWorkApplicationDynamicTable } =
-    await initDynamicTable(extraWorkApplicationTypeEnum);
-  const { extraWorkApplicationDynamicFormView } = await initDynamicFormView(
+  const { extraWorkApplicationDynamicForm, textCollectionDynamicForm } =
+    await initDynamicForm();
+  const {
+    userDynamicTable,
     extraWorkApplicationDynamicTable,
-    extraWorkApplicationDynamicForm,
-  );
-  const { employeeMenu } = await initMenu(extraWorkApplicationDynamicFormView);
+    textCollectionDynamicTable,
+  } = await initDynamicTable(extraWorkApplicationTypeEnum);
+  const { extraWorkApplicationDynamicFormView, textCollectionDynamicFormView } =
+    await initDynamicFormView(
+      { extraWorkApplicationDynamicTable, textCollectionDynamicTable },
+      { extraWorkApplicationDynamicForm, textCollectionDynamicForm },
+    );
+  const { employeeMenu } = await initMenu({
+    extraWorkApplicationDynamicFormView,
+    textCollectionDynamicFormView,
+  });
   const { testRole } = await initRole(testUser, employeeMenu);
+  await initDynamicTableRecored();
 }
 
 main()
